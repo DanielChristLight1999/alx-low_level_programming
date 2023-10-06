f6ff393bf27bfdcf097c82269a3bf768afc87509
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-06 16:34:02 -0700

    Added alphabet printer program more correction

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,15 +3,18 @@
  * main - Entry point of the program
  * This program uses putchar to print
  * all the alphabets in lowercase
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char letter = 'a';
+
 	while (letter <= 'z')
 	{
 		putchar(letter);
 		letter++;
 	}
+
 	putchar('\n');
 	return (0);
 }
