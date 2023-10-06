0683361dd04572662b24869ae49a9e239d68235b
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-06 16:49:25 -0700

    More alphabet programs added

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This program prints lowercase letters
+ * then uppercase letters using putchar
+ *
+ * Return - Always 0 (Success)
+ */
+int main(void)
+{
+	char uppercase = 'A';
+	
+	char lowercase = 'a';
+
+	while(lowercase <= 'z')
+	{
+		putchar(lowercase);
+		lowercase++;
+	}
+
+	while(uppercase <= 'Z')
+	{
+		putchar(uppercase);
+		uppercase++;
+	}
+
+	putchar('\n');
+}
