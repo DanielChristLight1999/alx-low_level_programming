36d5bfa8a4bb52928247b1c9c32b6510713b50d5
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-06 18:44:02 -0700

    Added base 16 printer

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ * main - Entry to this program
+ *
+ * This program prints all the base sixteen characters
+ * in lower case
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char letter = 'a';
+
+	char number = 0;
+
+	while (number <= 9)
+	{
+		putchar(number + '0');
+		number++
+	}
+
+	while (letter <= 'e')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
