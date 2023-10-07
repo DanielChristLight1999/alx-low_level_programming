b7076941ceec05113ce1605a4bc5d02374eb073b
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-07 02:24:41 -0700

    Added another combo program

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of this program
+ *
+ * This program prints the combination of three possible
+ * numbers without repition
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ones;
+	int tens;
+	int hunds;
+
+	for (ones = 0; ones <= 7; ones++)
+	{
+		for (tens = ones + 1; tens <= 8; tens++)
+		{
+			for (hunds = tens + 1; hunds <= 9; hunds++)
+			{
+				putchar(ones + '0');
+				putchar(tens + '0');
+				putchar(hunds + '0');
+
+				if (!(ones == 7 && tens == 8 && hunds == 9))
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+				
