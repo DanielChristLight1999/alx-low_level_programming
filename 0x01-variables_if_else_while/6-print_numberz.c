b01cdf852ff71201cb4f911d86e4026dd7241f8a
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-06 18:07:27 -0700

    New number printer

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio>;
+
+/**
+ * main - Entry point of program
+ *
+ * This program uses putchar to print numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char number = '0';
+
+	for (number = '0', number <= '9', number++)
+	{
+		putchar(number);
+	}
+
+	return (0);
+}
