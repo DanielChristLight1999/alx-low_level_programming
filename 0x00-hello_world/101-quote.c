1f10041f25a7eb069902dcffac6805108b49ca4c
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-07 11:23:28 -0700

    Added quote program

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of this program
+ *
+ * This program prints without printf or puts
+ *
+ * Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+	char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	
+	write(2, quote, 59);
+
+	return 1;
+}
