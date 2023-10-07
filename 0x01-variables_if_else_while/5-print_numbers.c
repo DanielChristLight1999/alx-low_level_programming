571a1b0f5e3c61c63153b8a73b30d99f5dfa807c
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-06 17:55:02 -0700

    Added program that print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ *
+ * This program prints all the numbers of base ten
+ * from zero to nine
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int number = 0;
+
+	while (number <= 9)
+	{
+		printf(number);
+		number++;
+	}
+
+	printf('\n');
+
+	return (0);
+}
