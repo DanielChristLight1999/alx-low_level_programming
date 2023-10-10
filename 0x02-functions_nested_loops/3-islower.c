4f7cdaa1e348a9bd44a71f8b00d54b7e31643414
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-10 12:23:15 -0700

    Added lower case checker

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _islower - Entry point to this program
+ *
+ * This program checks if the input is lowercase
+ *
+ * Return: 1 if lowercase and 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if (islower(c))
+	{
+		return (1);
+	} else {
+		return (0);
+	}
+}
