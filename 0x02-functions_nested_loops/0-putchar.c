c72c718b1907a6b5a020cc51ee4d07cddfd45739
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-10 07:25:16 -0700

    Started a new task

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of this program
+ *
+ * Thid program prints _putchar
+ *
+ * Return: Always 0(Success)
+ */
+
+int main()
+{
+	printf("_putchar\n");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Directory for learning functions and nested loops
