86247b4d09da0a1415e9b041329c193a83e6177c
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-10 12:08:39 -0700

    Added 10x printer

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Entry point of this program
+ *
+ * This program prints the alphabets ten times
+ *
+ * Return: Always void (Success)
+ */
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 0, i <= 10, i++)
+	{
+		char letter = 'a';
+
+		while (letter <= 'z')
+		{
+			_putchar(letter);
+
+			letter++;
+		}
+
+		_putchar('\n');
+	}
+}
