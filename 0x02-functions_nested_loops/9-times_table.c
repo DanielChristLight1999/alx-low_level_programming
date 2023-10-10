c8123d96110855ccda95397a9635c0a80e13c9db
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-10 16:44:22 -0700

    correction

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,40 +5,30 @@
  */
 void times_table(void)
 {
-    int row, column, product;
+	int row, column, product;
 
-    for (row = 0; row <= 9; row++)
-    {
-        for (column = 0; column <= 9; column++)
-        {
-            product = row * column;
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <=9; column++)
+		{
+			product = row * column;
 
-            if (column > 0)
-            {
-                _putchar(',');
-                _putchar(' ');
+			if (product >= 10)
+			{
+				_putchar((product / 10) + '0');
+				_putchar((product % 10) + '0');
+			}
+			else
+				_putchar(product + '10');
 
-                if (product < 10)
-                    _putchar(' ');
-            }
-
-            if (product < 10 && column > 0)
-                _putchar(' ');
-
-            if (product >= 10)
-            {
-                _putchar((product / 10) + '0');
-                _putchar((product % 10) + '0');
-            }
-            else
-            {
-                _putchar(' ');
-                _putchar(product + '0');
-            }
-
-            if (column == 9)
-                _putchar('\n');
-        }
-    }
+			if (column != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
 }
+
 
