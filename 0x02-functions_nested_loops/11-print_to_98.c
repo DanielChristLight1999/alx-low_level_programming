ae73810e39d76794ae5c2c5266470a86fb1a8ebc
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-10 21:27:18 -0700

    correction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -27,6 +27,16 @@ void print_to_98(int n)
 	_putchar('\n');
 }
 
+/**
+ * print_number - The prints numbers using putchar.
+ *
+ * @n: number to be printed.
+ *
+ * This program takes a number and prints as characters.
+ *
+ * Return:Always void
+ */
+
 void print_number(int n)
 {
 	if (n >= 0 && n <= 9)
