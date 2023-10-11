608bc99a47c6cacc14772d69c0fcce8beed88c80
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-11 07:03:09 -0700

    added 103-fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point of this program
+ *
+ * This program prints the sum of the even fibonacci numbers
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned long fib1 = 1;
+	unsigned long fib2 = 2;
+	unsigned long limit = 4000000;
+	unsigned long fibsum = 0;
+	unsigned long currentfib;
+
+	while(currentfib < limit)
+	{
+		currentfib = fib1 + fib2;
+
+		if (currentfib % 2 == 0)
+			fibsum += currentfib;
+		fib1 = fib2;
+		fib2 = currentfib;
+	}
+
+	printf("%lu\n", fibsum);
+
+	return (0);
+}
