d3c380deb32633f7ab4b1a2887649beac994a5d8
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-10 21:58:31 -0700

    correction

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -39,3 +39,29 @@ void print_times_table(int n)
 		_putchar('\n');
 	}
 }
+
+/**
+ * print_number - The prints numbers using putchar.
+ *
+ * @n: number to be printed.
+ *
+ * This program takes a number and prints as characters.
+ *
+ * Return:Always void
+ */
+
+void print_number(int n)
+{
+	if (n >= 0 && n <= 9)
+		_putchar(n + '0');
+	else if (n >= 10)
+	{
+		print_number(n / 10);
+		print_number(n % 10);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		print_number(-n);
+	}
+}
