d7a916731b051c9a0a120dc7542f65a54c407a89
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-10 18:55:20 -0700

    correction

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,23 +11,22 @@
  * Return: Always void
  */
 
-
 void print_number(int n)
 {
-    if (n >= 0 && n <= 9)
-    {
-        _putchar(n + '0');
-    }
-    else if (n >= 10)
-    {
-        print_number(n / 10);
-        print_number(n % 10);
-    }
-    else if (n < 0)
-    {
-        _putchar('-');
-        print_number(-n);
-    }
+	if (n >= 0 && n <=9)
+	{
+		_putchar(n + '0');
+	}
+	else if (n >= 10)
+	{
+		print_number(n / 10);
+		print_number(n % 10);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		print_number(-n);
+	}
 }
 
 void print_to_98(int n)
