5b9399bb703cad4c9d9e225929bf7f017f14b5f8
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-10 20:13:03 -0700

    corrections

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,16 +17,37 @@ void print_to_98(int n)
 	{
 		for (i = n; i < 98; i++)
 		{
-			_putchar(i + '0');
-			_putchar(',');
-			_putchar(' ');
+			if (i >= 0 && i < 10)
+			{
+				_putchar(i + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
+			else if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
 	}
 	else
 	{
 		for (i = n; i > 98; i--)
 		{
-			_putchar(i + '0');
+			if (i >= 100)
+			{
+				_putchar((i / 100) + '0');
+				_putchar(((i / 10) % 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+			else
+			{
+				_putchar((i / 10) + '0');
+				_putchar((i % 10) + '0');
+			}
+
 			_putchar(',');
 			_putchar(' ');
 		}
