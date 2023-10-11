ca68897b499126f869caec98782e5be40b27eed4
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-10 18:28:15 -0700

    added natural numbers printer

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include "main"
+
+/**
+ * print_to_98 - Prints all natural numbers from @n to 98
+ *
+ * print_number - prints a number
+ *
+ * @n: number to be checked
+ *
+ * Return: Always void
+ */
+
+
+void print_number(int n)
+{
+    if (n >= 0 && n <= 9)
+    {
+        _putchar(n + '0');
+    }
+    else if (n >= 10)
+    {
+        print_number(n / 10);
+        print_number(n % 10);
+    }
+    else if (n < 0)
+    {
+        _putchar('-');
+        print_number(-n);
+    }
+}
+
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = n; i != 98; i > 98 ? i-- : i++)
+	{
+		print_number(i);
+
+		if (i != 98)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+
+	putchar('\n');
+}
