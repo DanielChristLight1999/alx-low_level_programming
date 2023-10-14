d90a71e0ab51d3cf1fa417590508333fa3e9285e
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-13 21:29:09 -0700

    correction

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -12,13 +12,17 @@ void more_numbers(void)
 {
 	int a, i;
 
-	for (i = 0; i <= 10; i++)
+	for (i = 0; i < 10; i++)
 	{
 		for (a = 0; a <= 14; a++)
 		{
 			if (a >= 10)
+			{
 				_putchar('0' + (a / 10));
-			_putchar('0' + (a % 10));
+				_putchar('0' + (a % 10));
+			}
+			else
+				_putchar('0' + a);
 		}
 
 		_putchar('\n');
