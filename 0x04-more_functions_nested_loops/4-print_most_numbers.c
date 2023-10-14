1ae3f1600159804484b689dcf3528d66b8873adf
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-13 18:15:27 -0700

    Added 4-print_most_numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - This function prints except 
+ *
+ * Return: Always return void
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (!(i == 2 || i == 4))
+			_putchar(i + '0');
+	}
+
+	_putchar('\n');
+}
