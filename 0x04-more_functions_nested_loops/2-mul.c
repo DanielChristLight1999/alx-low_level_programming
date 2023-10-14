a59b3b773f7d7ec5a01d1d2a84cf4ddce8fc07d7
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-13 18:02:46 -0700

    mul added

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "main.h"
+
+/**
+ * mul - This function multiplies to variables
+ *
+ * @a: number to be mulitplied
+ *
+ * @b: number to be multiplied
+ *
+ * Return: The product of @a and @b
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
