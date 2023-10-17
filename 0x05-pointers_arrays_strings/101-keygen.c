528e16f398dd78509cb2274659c167a0d0b47593
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-17 10:59:43 -0700

    Add 101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,33 @@
+#include <time.h>
+
+#define PASSWORD_LENGTH 12
+
+/**
+ * main - Entry point of program
+ *
+ * This program generates random keys
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	char password[PASSWORD_LENGTH + 1];
+	int i;
+
+	srand(time(NULL));
+
+	for (i = 0; i < PASSWORD_LENGTH; i++)
+	{
+		int index = rand() % (sizeof(charset) - 1);
+
+		password[i] = charset[index];
+	}
+
+	password[PASSWORD_LENGTH] = '\0';
+
+	printf("Generated password: %s\n", password);
+
+	return (0);
+}
