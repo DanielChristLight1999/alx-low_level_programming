a65538319c518f5cb502bc92056b748cadf44844
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-16 19:52:49 -0700

    Add 3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts - This function prints a string to stdout
+ *
+ * @str: the string to be printed
+ *
+ * Return: Always void
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+}
