ff1a60b2f236c6d63b0a59d22ffe610f5be4bf4c
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-17 09:33:00 -0700

    Add 9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcpy - copies string
+ *
+ * @dest: The destination string
+ * @src: source string
+ *
+ * Return: The value pointer to @dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	return (strcpy(dest, src));
+}
