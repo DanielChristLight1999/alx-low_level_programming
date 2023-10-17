6125515eb1b2318ad6074d4d7eebb57e02333c0d
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-17 10:15:36 -0700

    correction

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -20,7 +20,13 @@ int _atoi(char *s)
 		if (s[i] == '-')
 			sign *= -1;
 		else if (s[i] >= '0' && s[i] <= '9')
+		{
+			if (result > (INT_MAX - (s[i] - '0')) / 10)
+			{
+				return ((sign == 1) ? INT_MAX : INT_MIN);
+			}
 			result = result * 10 + (s[i] - '0');
+		}
 		else if (result != 0)
 			break;
 	}
