fae81735f314f14f43d997852d91fbf67b427d98
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-17 02:30:02 -0700

    Add 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - prints every other character of a string
+ *
+ * @str: The string to be checked
+ *
+ * Return: Always void
+ */
+
+void puts2(char *str)
+{
+	int length = strlen(str);
+	int i;
+
+	for (i = 0; i <= length; i += 2)
+	{
+		_putchar(str[i]);
+	}
+}
