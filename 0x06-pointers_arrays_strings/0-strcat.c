4d4228086953787530f9a169b043810ca40a63c1
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-19 13:52:04 -0700

    Add 0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - Concatenates two strings
+ *
+ * @dest: the destination string
+ * @src: the source string
+ *
+ * Return: The concatenation of @dest and @src
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
