23db3e1c5820f37e0e47e9a44bbc6872b916638e
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-19 15:54:44 -0700

    Add 5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * string_toupper - Converts string to uppercase
+ *
+ * @str - The string to be converted
+ *
+ * Return: The converted string @str
+ */
+
+char *string_toupper(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+	return (*str);
+}
