883003f09857584d1e153a1be4b24ebf139e3488
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-19 14:22:23 -0700

    Add 3-strcmp.c

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcmp - Compares two strings
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: returns the difference between @s1 and @s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
