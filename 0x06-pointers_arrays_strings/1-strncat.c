91af7137a3fdafbbb6669940ab481c6e6b875ddf
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-19 14:02:40 -0700

    Add 1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strncat - Concactenates to strings
+ *
+ * @dest: the destination string
+ * @src: the source string
+ * @n: the number
+ *
+ * Return: A pointer to the result of concatenation of @dest and @src
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, 15));
+}
