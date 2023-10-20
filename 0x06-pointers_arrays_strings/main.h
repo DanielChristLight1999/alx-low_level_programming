3b3e678e187e28337ca4f3e4098932b6123c592b
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-19 18:18:11 -0700

    Add 103-infinite_add.c

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,4 +14,6 @@ char *cap_string(char *);
 char *leet(char *n);
 char *rot13(char *);
 void print_number(int n);
+void rev_string(char *n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
 #endif
