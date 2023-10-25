3a503bb49c5f47ebbb86b9f0d39832bb89652739
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-25 16:10:54 -0700

    new

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _puts_recursion - prints a string and a new line
+ *
+ * @s: The string to be printed
+ *
+ * Return: Always void
+ */
+
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
