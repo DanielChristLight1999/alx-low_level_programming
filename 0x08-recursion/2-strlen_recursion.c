f76ae3e3ff02f61d6d6e4f00004b40b2ea7d742a
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-10-25 16:20:25 -0700

    Add 2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include <string.h>
+
+/**
+ * _strlen_recursion - Returns the length of a string
+ *
+ * @s: the string to be checked
+ *
+ * Return: the length of @s
+ */
+
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
