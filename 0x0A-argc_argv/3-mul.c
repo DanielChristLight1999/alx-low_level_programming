b35c8b6d66a54bacd8921a6e47c712245faca696
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-11-01 19:05:40 -0700

    correction

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,29 +1,73 @@
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - Entry point of this program
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
  *
- * @a: First integer
- * @b: second integer
- *
- * Description: This function prints the product of two numbers
- *
- * Return: 0 if success. 1 if fail.
+ * Return: the int converted from the string
  */
-
-int main(int a, int b)
+int _atoi(char *s)
 {
-	int sum;
+	int i, d, n, len, f, digit;
 
-	if (!(a || b))
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+/**
+ * main - multiplies two numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0 (Success), 1 (Error)
+ */
+int main(int argc, char *argv[])
+{
+	int result, num1, num2;
+
+	if (argc < 3 || argc > 3)
 	{
 		printf("Error\n");
-
 		return (1);
 	}
 
-	sum = a * b;
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	result = num1 * num2;
 
-	printf("%d\n", sum);
+	printf("%d\n", result);
+
 	return (0);
 }
