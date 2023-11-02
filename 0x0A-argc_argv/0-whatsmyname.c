2b130e25ab9fbe33389a300cabee2f014a3331b1
DanielChristLight1999 <danielifesinachi0@gmail.com>
2023-11-01 18:03:49 -0700

    Add 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of this program
+ *
+ * @argc: Number of inputs in command
+ * @argv: array of string of commands
+ *
+ * Return: Always (0) Success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+
+	return (0);
+}
